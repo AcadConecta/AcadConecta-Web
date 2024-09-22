4c58454058d8cb113d106771667fe7ba48ee81ba
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2024-09-22 11:43:03 -0300

    first commit

---
+++ src/main/java/code/acadConecta/AcadConectaApplication.java
@@ -0,0 +1,13 @@
+package code.acadConecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadConectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadConectaApplication.class, args);
+	}
+
+}
